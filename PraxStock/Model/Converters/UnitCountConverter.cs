22050ea6c860fc9363147ce0f58dabf49a8c0f93
Igor Radkevich <landrommigor@gmail.com>
2024-10-10 15:55:47 +0300

    Добавлено отображение флага вкючения контроля

--- PraxStock/Model/Converters/UnitCountConverter.cs
+++ PraxStock/Model/Converters/UnitCountConverter.cs
@@ -10,14 +10,25 @@ namespace PraxStock.Model.Converters
 {
 	public class UnitCountConverter : IMultiValueConverter
 	{
+		enum MyEnum
+		{
+			Green = 1,
+			Red = 2,
+			Gray = 3
+		}
 
 		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
 		{
 			try
 			{
-				if (System.Convert.ToDouble(values[0]) <= System.Convert.ToDouble(values[1]))
-					return true;
-				return false;
+				if(System.Convert.ToBoolean(values[2])) 
+				{
+					if (System.Convert.ToDouble(values[0]) <= System.Convert.ToDouble(values[1])
+						&& System.Convert.ToDouble(values[0]) > 0)
+						return MyEnum.Green;
+					return MyEnum.Red;
+				}
+				return MyEnum.Gray;
 			}
 			catch (Exception e)
 			{
