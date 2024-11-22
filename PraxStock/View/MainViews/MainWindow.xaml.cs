9ec7163aa16213c7b85735b3e64a206bebd047a7
Igor Radkevich <landrommigor@gmail.com>
2024-11-22 12:05:00 +0300

    Добавление копирования в буффер строк и основной таблицы.

--- PraxStock/View/MainViews/MainWindow.xaml.cs
+++ PraxStock/View/MainViews/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System.Globalization;
+﻿using PraxStock.Model.OtherModel;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -16,8 +19,47 @@ namespace PraxStock.View.MainViews;
 /// </summary>
 public partial class MainWindow : Window
 {
+	private MainListItems _mainListItems;
+
 	public MainWindow()
 	{
 		InitializeComponent();
+		_mainListItems = new();
+	}
+
+	private void CopyInBuffer_Click(object sender, RoutedEventArgs e)
+	{
+		CopyToCsv();
+	}
+
+	private void dataStockListMain_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+	{
+		if (e.KeyboardDevice.Modifiers == ModifierKeys.Control && e.Key == Key.C)
+		{
+			CopyToCsv();
+		}
+	}
+
+	private void CopyToCsv()
+	{
+		if (dataStockListMain.SelectedItems != null)
+		{
+			System.Windows.Clipboard.Clear();
+			var exportList = new List<List<string>>();
+			var subExportList = new List<string>();
+			string? str = "";
+			for (int i = 0; i < dataStockListMain.SelectedItems.Count; i++)
+			{
+				var convertObject = (MainListItems)dataStockListMain.SelectedItems[i];
+				str += convertObject.Name;
+				str += "\t";
+				str += convertObject.UnitCount;
+				str += "\t";
+				str += convertObject.UnitCount;
+				if (i != dataStockListMain.SelectedItems.Count - 1)
+					str += "\n";
+			}
+			System.Windows.Clipboard.SetText(str);
+		}
 	}
 }
